6dcc01bf0fc018853f4a0fde5fdc022c75b0f027
edcamero <39476414+edcamero@users.noreply.github.com>
2019-12-23 05:02:34 -0500

    Navbar esta siendo usado y navega sin problemas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GraficaHorizontalComponent } from './grafica-horizontal/grafica-horizontal.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [NavbarComponent,GraficaHorizontalComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     NavbarComponent,GraficaHorizontalComponent
