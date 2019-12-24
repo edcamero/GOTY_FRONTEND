47c00be2644863c7ba68e42b941c3b6fb3c74593
edcamero <39476414+edcamero@users.noreply.github.com>
2019-12-24 05:00:12 -0500

    Funcionando barra de graficas con datos ramdon

--- src/app/components/grafica-horizontal/grafica-horizontal.component.ts
+++ src/app/components/grafica-horizontal/grafica-horizontal.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-grafica-horizontal',
   templateUrl: './grafica-horizontal.component.html',
   styleUrls: ['./grafica-horizontal.component.css']
 })
-export class GraficaHorizontalComponent {
-  results: any[];
+export class GraficaHorizontalComponent implements OnDestroy {
+  results: any[]=[
+    {
+      "name": "Juego 1",
+      "value": 20
+    },
+    {
+      "name": "Juego 2",
+      "value": 25
+    },
+    {
+      "name": "Juego 3",
+      "value": 40
+    }
+    ,
+    {
+      "name": "Juego 4",
+      "value": 10
+    }
+  ];
 
 
-  view: any[] = [700, 400];
+ 
 
   // options
   showXAxis = true;
@@ -25,10 +43,28 @@ export class GraficaHorizontalComponent {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
   };
 
+  intervalo;
   constructor() {
+
+
+    this.intervalo=setInterval(()=>{
+      const newRerults=[...this.results]
+      for (let i in newRerults){
+        newRerults[i].value=Math.round(Math.random()*100);
+      }
+
+
+      this.results=[...newRerults]
+    },1500);
+    
   }
 
   onSelect(event) {
     console.log(event);
+  }
+
+
+  ngOnDestroy(){
+    clearInterval(this.intervalo);
   }
 }
